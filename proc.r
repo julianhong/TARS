8b59dd3e411c43404a6edba3aa957f1e5bc0c342
Julian Hong <5290392+julianhong@users.noreply.github.com>
2017-12-19 13:58:43 -0500

    Procedures
    proc.r - history of procedures by AHRQ level 3

---
+++ proc.r
@@ -0,0 +1,31 @@
+#proc.r
+#sort out procedures and generate a wide table for procedure history
+
+patientproc <- distinct(bind_rows(patientprocedures20132014.csv, patientprocedures20152016.csv))
+
+#convert the dates
+patientproc$Procedure.Date <- as.Date(patientproc$Procedure.Date , "%m/%d/%Y %H:%M:%S")
+
+#need to see relative to the time of starting radiation though
+#rttime from the genpatient code to generate start and end dates
+temp <- left_join(patientproc, rttime, by="Patient.Identifier")
+
+#now some timing things
+temp$daysprior <- temp$start - temp$Procedure.Date
+
+#filter to the past
+temp <- filter(temp, daysprior > -1)
+
+#now consolidate the different codes; level 1 seems too broad; the subcategory or level 2 or 3 seem good
+#use level 3 for now since i think that's a good start
+wideprocicd <- select(temp, Patient.Identifier, course, AHRQ.Procedure.Level.3.Text)
+wideprocicd <- distinct(wideprocicd) #drop duplicates
+wideprocicd <- filter(wideprocicd, AHRQ.Procedure.Level.3.Text != "")
+
+#go from long to wide format
+library(tidyr)
+wideprocicd$present <- 1
+wideprocicd <- spread(wideprocicd, AHRQ.Procedure.Level.3.Text, present)
+wideprocicd[is.na(wideprocicd)] <- 0 #need to fill in the NAs
+
+rm(temp) #cleanup
